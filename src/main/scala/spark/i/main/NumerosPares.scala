5a7df3427e6e0159347c26417ba20f2c2813487d
damarcant <damarcant@gmail.com>
2016-02-18 19:54:15 +0100

    First commit

---
+++ src/main/scala/spark/i/main/NumerosPares.scala
@@ -0,0 +1,21 @@
+package spark.i.main;
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object NumerosPares extends App {
+
+  val sparkConf = new SparkConf().setAppName("NumberAnalysis").setMaster("local")
+  val sc = new SparkContext(sparkConf)
+
+  val listaNumeros = (1 to 10000).toList
+
+  val listaNumerosRdd = sc.parallelize(listaNumeros)
+    
+  val listaNumerosParesRdd = listaNumerosRdd.filter{ _ % 2 == 0}
+  
+  val count = listaNumerosParesRdd.count
+  
+  println("Cantidad de números pares encontrados: "+count);
+  
+}
